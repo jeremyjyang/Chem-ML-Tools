35f42a9af105a7dee5e568ac99d9c223acfc5052
Jeremy Yang <jeremyjyang@gmail.com>
2019-10-23 11:12:24 -0400

    Ok.

--- sh/Go_deepchem_Test.sh
+++ sh/Go_deepchem_Test.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 #
 # https://deepchem.io/
 # https://github.com/deepchem/deepchem
 ###
 # Karst: https://kb.iu.edu/d/bezu
 # Modules: https://kb.iu.edu/d/bcwy
+###
+#
+cwd=$(pwd)
+#
+#
 ###
 module list
 module avail anaconda3
@@ -19,6 +24,14 @@ source activate deepchem_env
 conda info
 conda list
 #
+###
+# Test RDKit
+${cwd}/python/rdk_fptest.py
+#
+###
+# Test DeepChem...
+
+
 ###
 # Deactivate DeepChem env.
 source activate root
