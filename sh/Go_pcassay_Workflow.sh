e71dd0b94b514c1127c398ca8e1ca39438b0d165
Jeremy Yang <jeremyjyang@gmail.com>
2021-10-28 13:11:40 -0600

    Alright.

--- sh/Go_pcassay_Workflow.sh
+++ sh/Go_pcassay_Workflow.sh
@@ -23,19 +23,26 @@ python3 -m BioClients.pubchem.Client get_assaysubstanceresults \
 	--aid ${AID} --i ${DATADIR}/aid_${AID}_substances.sid \
 	--o ${DATADIR}/aid_${AID}_results.tsv
 #
-# Columns: SMILES, CID
+# Columns: CID,CanonicalSMILES,IsomericSMILES
 python3 -m BioClients.pubchem.Client get_cid2smiles \
 	--i ${DATADIR}/aid_${AID}_compounds.cid \
 	--o ${DATADIR}/aid_${AID}_compounds.smiles
+python3 -m BioClients.util.pandas.Utils list_columns --i ${DATADIR}/aid_${AID}_compounds.smiles
 #
 # Columns: 
 python3 -m BioClients.pubchem.Client get_cid2properties \
 	--i ${DATADIR}/aid_${AID}_compounds.cid \
 	--o ${DATADIR}/aid_${AID}_compounds_properties.tsv
 #
-python3 -m BioClients.util.pandas.Utils showcols --i ${DATADIR}/aid_${AID}_compounds_properties.tsv
-python3 -m BioClients.util.pandas.Utils summary --i ${DATADIR}/aid_${AID}_compounds_properties.tsv
+python3 -m BioClients.util.pandas.Utils list_columns --i ${DATADIR}/aid_${AID}_compounds_properties.tsv
 #
 ###
 # RDKit
 ###
+python3 -m rdktools.fp.App FingerprintMols --i ${DATADIR}/aid_${AID}_compounds.smiles \
+	--smilesColumn 1 --nameColumn 0 \
+	--fpAlgo MACCS \
+	--o ${DATADIR}/aid_${AID}_compounds_MaccsFps.pkl
+#
+###
+# Combine fps and activity labels into ML-ready X and Y.
